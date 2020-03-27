4574226bcd7286bdfff52e3bc21dcd2f26fa8c7e
Dongjianzheng <1175639137@qq.com>
2020-03-27 19:29:39 +0800

    重新生成实体类

--- src/main/java/com/djz/self/entity/basic/mapper/RoleResourceMapper.java
+++
@@ -1,9 +0,0 @@
-package com.djz.self.entity.basic.mapper;
-
-import com.djz.self.entity.basic.RoleResource;
-
-public interface RoleResourceMapper {
-    int insert(RoleResource record);
-
-    int insertSelective(RoleResource record);
-}
--- src/main/java/com/djz/self/entity/basic/mapper/UserRoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.djz.self.entity.basic.mapper;
-
-import com.djz.self.entity.basic.UserRole;
-
-public interface UserRoleMapper {
-    int insert(UserRole record);
-
-    int insertSelective(UserRole record);
-}
